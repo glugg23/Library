fdded04471581f7ece7c9a31f77217455cd0de06
glugg23 <m.leonhardt424@gmail.com>
2018-06-05 22:53:01 +0100

    Added code to make initial connection

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 import java.sql.*;
+import java.util.Properties;
+import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
+        Connection connection = null;
+        Properties login = new Properties();
 
+        try(FileReader in = new FileReader("login.properties")) {
+            login.load(in);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(login.getProperty("server"), login.getProperty("username"), login.getProperty("password"));
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
