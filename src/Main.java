23230d3a3080053f8af44c8d742102174ade801d
glugg23 <m.leonhardt424@gmail.com>
2018-06-05 23:39:59 +0100

    Started to add code to allow valid users to login

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.sql.*;
 import java.util.Properties;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,5 +21,33 @@ public class Main {
         } catch(Exception e) {
             e.printStackTrace();
         }
+
+        User user;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter your username: ");
+            String username = in.nextLine();
+            System.out.print("Enter you password: ");
+            String password = in.nextLine();
+
+            user = new User(username, password);
+
+            String query = "SELECT * FROM users " +
+                           "WHERE username='" + username + "' AND password='" + password + "';";
+            try {
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+
+                if (!resultSet.isBeforeFirst() ) {
+                    System.out.println("No data");
+                }
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } while(!user.isLoggedIn());
     }
 }
