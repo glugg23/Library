96d57649b7550a4556262a4667212d16b78b8c42
glugg23 <m.leonhardt424@gmail.com>
2018-06-11 17:50:39 +0100

    Added returns menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.Statement;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -40,7 +41,7 @@ class Menu {
                     menuOpenedAgain = true;
                     break;
                 case 2:
-                    System.out.println("Return Menu");
+                    returnMenu(user, connection, in);
                     menuOpenedAgain = true;
                     break;
                 case 3:
@@ -59,5 +60,62 @@ class Menu {
             }
 
         } while(choice != 0);
+    }
+
+    static private void returnMenu(User user, Connection connection, Scanner in) {
+        System.out.println("Returns Menu");
+        if(user.getBook() != null) {
+            System.out.println("You have borrowed " + user.getBook().getTitle() + " by " + user.getBook().getAuthor());
+            System.out.println("\t1 - Return this book\n" +
+                               "\t0 - Go back to main menu\n");
+
+            int choice;
+
+            do {
+                System.out.print(user.getUsername() + ": ");
+                try {
+                    choice = in.nextInt();
+
+                } catch(InputMismatchException e) {
+                    //If the user enters something wrong set choice to a still valid number
+                    choice = -1;
+                }
+
+                switch(choice) {
+                    case 1:
+                        String queryBooks = "UPDATE books " +
+                                            "SET isBorrowed=0, borrowedBy=null, returnDate=null " +
+                                            "WHERE title='" + user.getBook().getTitle() + "';";
+
+                        String queryUsers = "UPDATE users " +
+                                            "SET book=null " +
+                                            "WHERE username='" + user.getUsername() + "';";
+
+                        try {
+                            Statement statement = connection.createStatement();
+                            statement.execute(queryBooks);
+                            statement.execute(queryUsers);
+
+                        } catch(Exception e) {
+                            System.out.println("There was an error, please try again.");
+                            e.printStackTrace();
+                        }
+
+                        System.out.println("Success!");
+                        return;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                        //Clear input buffer as this will trigger when the user enters invalid input
+                        in.nextLine();
+                        break;
+                }
+
+            } while(choice != 0);
+
+        } else {
+            System.out.println("You have not borrowed a book.\n");
+        }
     }
 }
